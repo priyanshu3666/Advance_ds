f6597047282caead82dde60368bbf40629f3cbf6
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-02-12 16:12:12 +0530

    initial

---
+++ SinglyLinkedList.java
@@ -0,0 +1,57 @@
+public class SinglyLinkedList
+{
+    private ListNode head ;
+    private static class ListNode
+    {
+        private int data;
+        private ListNode next;
+        public ListNode(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+        
+    }
+    
+    public int length()
+    {
+        if(head == null )
+        {
+            return 0;
+        }
+        int count = 0;
+        ListNode current = head;
+        while(current != null)
+        {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+    public void display()
+    {
+        ListNode current =  head;
+        while(current != null)
+        {
+            System.out.println(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("list is ended");
+        
+    }
+    public static void main(String[] args) {
+	    SinglyLinkedList  sll = new SinglyLinkedList();
+	    sll.head  = new ListNode(10);
+	    ListNode second = new ListNode(1);
+	    ListNode third = new ListNode(8);
+	    ListNode fourth = new ListNode(11);
+	    
+	    sll.head.next = second;
+	    second.next = third;
+	    third.next = fourth;
+	    ListNode current = sll.head;
+	    sll.display();
+	    System.out.println("the number of element in linked list = " + sll.length());
+	}
+    
+}
